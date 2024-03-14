f58a039f82c400c3cf03c7c0ab4b0ccef2bf5490
j-sylvia <150928883+j-sylvia@users.noreply.github.com>
2024-03-14 18:59:34 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,27 +1,40 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
-function Todo(){
-    var [input,setInput] = useState("");
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-    function add(){
-    // var inp=event.target.value;
-    alert(input);
-
-    
+  const addTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
     }
-    function remove(){
-        setInput("");
-        // input=" ";
-        alert("removed"); 
-    }
+  };
 
-    return (
-        <div>        
-            <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
-            <button onClick={add}>+</button>
-            <button onClick={remove}>-</button>
-            {/* <h3>{input}</h3> */}
-        </div>
-    )
+  const deleteTodo = index => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default Todo;
+
+export default TodoList;
